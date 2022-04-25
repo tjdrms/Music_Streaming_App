c1e76b40a834c02f48a483009616739618823162
tjdrms <tjdrms0116>
2022-04-25 11:06:12 +0900

    뮤직 스트리밍 어플리케이션

---
+++ app/src/main/java/com/example/music_streaming_app/MusicModelMapper.kt
@@ -0,0 +1,20 @@
+package com.example.music_streaming_app
+
+import com.example.music_streaming_app.service.MusicDto
+import com.example.music_streaming_app.service.MusicEntity
+
+fun MusicEntity.mapper(id: Long): MusicModel =
+    MusicModel(
+        id = id,
+        streamUrl = streamUrl,
+        coverUrl = coverUrl,
+        track = track,
+        artist = artist
+    )
+
+fun MusicDto.mapper(): PlayerModel =
+    PlayerModel(
+        playMusicList = musics.mapIndexed { index, musicEntity ->
+            musicEntity.mapper(index.toLong())
+        }
+    )
